59ec346e1662ea34286297fda6a525f3a4fa2c8d
Jesse <jesse.yegon@student.moringaschool.com>
2025-05-27 08:00:17 +0300

    new article

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -3,20 +3,20 @@ from lib.models.article import Article
 
 def seed():
  
-    a1 = Author(name="George Orwell")
+    a1 = Author(name="Greg Saita")
     a1.save()
 
-    a2 = Author(name="Virginia Woolf")
+    a2 = Author(name="Mutali Chesebe")
     a2.save()
 
    
-    art1 = Article(title="Politics and the English Language", author_id=a1.id, magazine_id=1)
+    art1 = Article(title="My life in crime", author_id=a1.id, magazine_id=1)
     art1.save()
 
-    art2 = Article(title="The Death of the Moth", author_id=a2.id, magazine_id=2)
+    art2 = Article(title="The Diary of a Wimpy kid", author_id=a2.id, magazine_id=2)
     art2.save()
 
-    print("Database seeded!")
+    print("Magazine seeded!")
 
 if __name__ == "__main__":
     seed()
